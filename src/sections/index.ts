e08f6be8df93bdb9eace36b6071e2d4109f4160d
Ibrahim Raimi <raimiibrahim44@gmail.com>
2025-07-21 11:46:22 +0100

    feat(hero): enhance Hero component with new layout, content, and image; add Button for call-to-action

---
+++ src/sections/index.ts
@@ -0,0 +1,9 @@
+import SiteHeader from "./site-header";
+import SiteFooter from "./site-footer";
+import Hero from "./hero";
+import Projects from "./projects";
+import Testimonials from "./testimonials";
+import FAQs from "./faq";
+import Intro from "./intro";
+
+export { SiteHeader, SiteFooter, Hero, Projects, Testimonials, FAQs, Intro };
