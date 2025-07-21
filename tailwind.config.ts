bd535c22175a48d4e2cc466c5bf96e6a8958a251
Ibrahim Raimi <raimiibrahim44@gmail.com>
2025-07-21 11:30:04 +0100

    refactor(tailwind): reorganize configuration and remove unused globals.css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,39 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    content: [
-        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ["var(--font-archivo)"],
-            },
-        },
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "1200px",
     },
-    plugins: [],
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-archivo)"],
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          md: "2rem",
+          lg: "4rem",
+        },
+      },
+      colors: {
+        black: "#000000",
+        charcoal: "#393632",
+        night: "#080807",
+        sage: "#8C8C73",
+        taupe: "#A29E9A",
+        bone: "#BFBFB1",
+      },
+    },
+  },
+  plugins: [],
 };
 export default config;
