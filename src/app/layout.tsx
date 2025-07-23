e8189acdff9f40d266a281bd4c212d768c7ddcb1
Ibrahim Raimi <raimiibrahim44@gmail.com>
2025-07-23 16:53:48 +0100

    feat(opengraph): add Open Graph image and update metadata for improved sharing feat(robots): add robots.txt for better SEO management feat(sitemap): create sitemap for enhanced site indexing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,13 +59,21 @@ export const metadata: Metadata = {
     url: siteConfig.url,
     title: siteConfig.name,
     description: siteConfig.description,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
     siteName: siteConfig.name,
   },
   twitter: {
     card: "summary_large_image",
     title: siteConfig.name,
     description: siteConfig.description,
-    images: [`${siteConfig.url}/og.png`],
+    images: [`${siteConfig.url}/opengraph-image.png`],
     creator: "@ibrahimraimi_",
   },
   icons: {
